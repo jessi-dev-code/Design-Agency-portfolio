fd4c2ff36d8647dd515b78d15fe88e0b3ab5ee7f
jaspreet <jaspreet.bhagat@ensuesoft.com>
2025-08-12 18:39:46 +0530

    Hover element added to the hero section

--- src/Components/HeroSection.tsx
+++ src/Components/HeroSection.tsx
@@ -1,5 +1,18 @@
 import { Box, Container, Typography } from "@mui/material"
 import { motion } from "framer-motion"
+import Hhover from "../assets/Hhover.png"
+import Ohover from "../assets/Ohover.png"
+import Uhover from '../assets/Uhover.png'
+import Shover from "../assets/Shover.png"
+import Ehover from "../assets/Ehover.png"
+import Ohover2 from "../assets/Ohover2.png"
+import Fhover from "../assets/Fhover.png"
+import Dhover from "../assets/Dhover.png"
+import Ehover2 from "../assets/Ehover(2).png"
+import Shover2 from "../assets/Shover(2).png"
+import Ihover from "../assets/Ihover.png"
+import Ghover from "../assets/Ghover.png"
+import Nhover from "../assets/Nhover.png"
 
 const HeroSection = () => {
   return (
@@ -31,22 +44,199 @@ const HeroSection = () => {
           initial={{ opacity: 0, y: 140 }}
           whileInView={{ opacity: 1, y: 10 }}
           transition={{ duration: 1.4, ease: "easeInOut" }}
-        className="flex justify-center mx-auto gap-[4px]">
-        <Box className="HeroHeadingText">H</Box>
-        <Box className="HeroHeadingText">O</Box>
-        <Box className="HeroHeadingText">U</Box>
-        <Box className="HeroHeadingText">S</Box>
-        <Box className="mr-[12px] HeroHeadingText">E</Box>
+        className="flex justify-center !mx-auto !ml-10 gap-[4px] overflow-x-hiddenx-hidden">
+          <Box className="relative">
+          <motion.img
+          initial={{
+            opacity:0
+          }}
+          whileHover={{
+            scale:1.25,
+            opacity:1
+          }}
+
+          src={Hhover} className="!h-[260px] !min-w-[200px] absolute !right-[-0%] bottom-[0%] z-40" />
+        <Box className="HeroHeadingText">H
+        </Box>
+          </Box>
+          <Box className="relative">
+          <motion.img
+          initial={{
+            // scale:1,
+            opacity:0
+          }}
+          whileHover={{
+            scale:1.25,
+            opacity:1
+          }}
+
+          src={Ohover} className="!h-[160px] !min-w-[160px] absolute !right-[-%] bottom-[20%] z-20" />
+        <Box className="HeroHeadingText">O
+        </Box>
+          </Box>
+          <Box className="relative">
+          <motion.img
+          initial={{
+            // scale:1,
+            opacity:0
+          }}
+          whileHover={{
+            scale:1.25,
+            opacity:1
+          }}
+
+          src={Uhover} className="!h-[300px] !min-w-[200px] absolute !right-[-10%] bottom-[8%] z-[8]" />
+        <Box className="HeroHeadingText">U
+        </Box>
+          </Box>
+          <Box className="relative">
+          <motion.img
+          initial={{
+            opacity:0
+          }}
+          whileHover={{
+            scale:1.25,
+            opacity:1
+          }}
+
+          src={Shover} className="!h-[240px] !min-w-[180px] absolute !right-[-0%] bottom-[10%] z-40" />
+        <Box className="HeroHeadingText">S
+        </Box>
+          </Box>
+          <Box className="relative">
+          <motion.img
+          initial={{
+            opacity:0
+          }}
+          whileHover={{
+            scale:1.25,
+            opacity:1
+          }}
+
+          src={Ehover} className="!h-[180px] !min-w-[200px] absolute !right-[-0%] bottom-[20%] z-40" />
+        <Box className="HeroHeadingText">E
+        </Box>
+          </Box>
+
         <div className="flex">
-        <Box className="HeroHeadingText">O</Box>
-        <Box className="HeroHeadingText">F</Box>
+          <Box className="relative">
+          <motion.img
+          initial={{
+            opacity:0
+          }}
+          whileHover={{
+            scale:1.25,
+            opacity:1
+          }}
+
+          src={Ohover2} className="!h-[140px] !min-w-[160px] absolute !right-[8%] bottom-[40%] z-40" />
+        <Box className="HeroHeadingText">O
+        </Box>
+          </Box>
+          <Box className="relative">
+          <motion.img
+          initial={{
+            opacity:0
+          }}
+          whileHover={{
+            scale:1.25,
+            opacity:1
+          }}
+
+          src={Fhover} className="!h-[200px] !min-w-[200px] absolute !right-[0%] bottom-[18%] z-40" />
+        <Box className="HeroHeadingText">F
+        </Box>
+          </Box>
         </div>
-        <Box className="ml-[12px] HeroHeadingText">D</Box>
-        <Box className="HeroHeadingText">E</Box>
-        <Box className="HeroHeadingText">S</Box>
-        <Box className="HeroHeadingText">I</Box>
-        <Box className="HeroHeadingText">G</Box>
-        <Box className="HeroHeadingText">N</Box>
+          <Box className="relative">
+          <motion.img
+          initial={{
+            opacity:0
+          }}
+          whileHover={{
+            scale:1.25,
+            opacity:1
+          }}
+
+          src={Dhover} className="!h-[200px] !min-w-[200px] absolute !right-[0%] bottom-[18%] z-[80]" />
+        <Box className="HeroHeadingText">D
+        </Box>
+          </Box>
+          <Box className="relative">
+          <motion.img
+          initial={{
+            opacity:0
+          }}
+          whileHover={{
+            scale:1.25,
+            opacity:1
+          }}
+
+          src={Ehover2} className="!h-[200px] !min-w-[360px] absolute !right-[-60%] bottom-[18%] z-40" />
+        <Box className="HeroHeadingText">E
+        </Box>
+          </Box>
+          <Box className="relative">
+          <motion.img
+          initial={{
+            // scale:1,
+            opacity:0
+          }}
+          whileHover={{
+            scale:1.25,
+            opacity:1
+          }}
+
+          src={Shover2} className="!h-[100px] !min-w-[160px] absolute !right-[-0%] bottom-[40%] z-50 !rotate-[-20deg]" />
+        <Box className="HeroHeadingText">S
+        </Box>
+          </Box>
+          <Box className="relative">
+          <motion.img
+          initial={{
+            // scale:1,
+            opacity:0
+          }}
+          whileHover={{
+            scale:1.25,
+            opacity:1
+          }}
+
+          src={Ihover} className="!h-[160px] !min-w-[180px] absolute !right-[-40%] bottom-[30%] z-[60]" />
+        <Box className="HeroHeadingText">I
+        </Box>
+          </Box>
+          <Box className="relative">
+          <motion.img
+          initial={{
+            // scale:1,
+            opacity:0
+          }}
+          whileHover={{
+            scale:1.25,
+            opacity:1
+          }}
+
+          src={Ghover} className="!h-[200px] !min-w-[280px] absolute !right-[-40%] bottom-[10%]" />
+        <Box className="HeroHeadingText">G
+        </Box>
+          </Box>
+          <Box className="relative">
+          <motion.img
+          initial={{
+            // scale:1,
+            opacity:0
+          }}
+          whileHover={{
+            scale:1.25,
+            opacity:1
+          }}
+
+          src={Nhover} className="!h-[260px] !min-w-[200px] absolute !right-[12%] bottom-[8%]" />
+        <Box className="HeroHeadingText">N
+        </Box>
+          </Box>
+
         </motion.div>
     </Container>
   )
